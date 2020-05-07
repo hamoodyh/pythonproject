8e83e1a8ccb7fe2d9242ec02723914058a4c7ccc
hamoodyh <60248010+hamoodyh@users.noreply.github.com>
2020-05-07 03:04:25 -0400

    Even and Odd count increments and prints

--- evenOdd.py
+++ evenOdd.py
@@ -15,4 +15,18 @@ for number in range(0, elementcount):
 
     tally.append(ele)  # adds the element to the tally
 
+
+# for loop that loops through the number tally
+for number in tally:
+
+    # use modulus operator to find the remainder of the number when divided by 2
+    if number % 2 == 0:    # if the remainder is 0
+        even = even + 1 # increment amount of even numbers
+
+    if number % 2 == 1:  # else if the remainder when divided by 2 is 1
+        odd = odd + 1   # increment amount of odd numbers
+
 print("The list you entered is: ", tally)
+
+print("Even numbers in the list: ", even)
+print("Odd numbers in the list: ", odd)
