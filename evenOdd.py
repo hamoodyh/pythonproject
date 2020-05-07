2591082d537416cddd37171ceb7001ec6c032a96
hamoodyh <60248010+hamoodyh@users.noreply.github.com>
2020-05-07 02:57:55 -0400

    loop for user input list and even and odd counts

--- evenOdd.py
+++ evenOdd.py
@@ -5,3 +5,14 @@ odd = 0
 # empty tally
 # empty tally created
 tally = []
+
+# number of elements as input
+elementcount = int(input("Enter the number of elements in your list : "))
+
+# for loop until the number of element is reached
+for number in range(0, elementcount):
+    ele = int(input())
+
+    tally.append(ele)  # adds the element to the tally
+
+print("The list you entered is: ", tally)
